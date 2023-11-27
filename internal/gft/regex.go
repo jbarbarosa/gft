ed0b18f9b234c659c60a58e133cb00e841e32b15
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2023-11-27 15:33:06 -0300

    regex cleans up test name

--- internal/gft/regex.go
+++ internal/gft/regex.go
@@ -1,5 +1,13 @@
 package gft
 
+import "strings"
+
 func Regex(testNames []string) string {
-	return "^Test(ShouldListTestNamesFromFiles|ShouldReturnErrorIfFileIsNotFound)$"
+	regex := "^Test("
+	for _, name := range testNames {
+		name = strings.TrimPrefix(name, "Test")
+		regex += name + "|"
+	}
+	regex = strings.TrimSuffix(regex, "|")
+	return regex + ")$"
 }

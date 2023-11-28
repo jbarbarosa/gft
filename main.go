acc8b48206f5ebed66bd2e5af48b3a2c1ed47b85
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2023-11-28 08:47:36 -0300

    creates executable

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/jbarbarosa/gft/internal/gft"
+)
+
+func main() {
+	tests, err := gft.TestsFromFile(os.Args[1])
+	if err != nil {
+		fmt.Println("unable to run tests from file")
+	}
+	regex := gft.Regex(tests)
+	fmt.Println("Running tests with the following regex:", regex)
+	exec := exec.Command("go", "test", "-run", regex, ".")
+	out, err := exec.Output()
+	if err != nil {
+		fmt.Println("error running tests: ", err)
+	}
+	fmt.Println(string(out))
+}
